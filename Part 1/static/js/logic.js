8f4f28d0242a412b684b8bf95dd353633ff9c771
Aaron Spivak <69207934+aspivak5@users.noreply.github.com>
2021-02-18 13:49:47 -0500

    Update logic.js

--- Part 1/static/js/logic.js
+++ Part 1/static/js/logic.js
@@ -1,9 +1,16 @@
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
+// var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//   tileSize: 512,
+//   maxZoom: 18,
+//   zoomOffset: -1,
+//   id: "mapbox/streets-v11",
+//   accessToken: API_KEY
+// });
+
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
+  id: "light-v10",
   accessToken: API_KEY
 });
 
@@ -14,14 +21,14 @@ var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z
   accessToken: API_KEY
 });
 var baseMaps = {
-    "Street Map": streetmap,
+    "Street Map": lightmap,
     "Dark Map": darkmap
   };
 
   var myMap = L.map("map", {
     center: [32.09, -100.71],
     zoom: 4,
-    layers: [streetmap]
+    layers: [lightmap]
   });
   
   // Pass our map layers into our layer control
@@ -33,7 +40,6 @@ var baseMaps = {
   var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
   
   d3.json(url, function(data) {
-    // Creating a GeoJSON layer with the retrieved data
     console.log(data)
     function Choosecolor(depth){
         if (depth > 90) {
@@ -74,6 +80,11 @@ var baseMaps = {
                 color: "black",
                 weight:1
             }
+        },
+        onEachFeature: function(feature,layer){
+            layer.bindPopup(
+                `Location: ${feature.properties.place} <br> Magnitude: ${feature.properties.mag} <br> Depth: ${feature.geometry.coordinates[2]}`
+            )
         }
     }).addTo(myMap)
 
